aeafea6e18d10e99052ece02088a4f0d090f6012
ahmprs <parszadeh.ahmad@gmail.com>
2024-04-05 11:00:05 -0700

    Test Ok

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+// ## Note:
+// Head over to https://pdfkit.org/docs/guide.pdf
+
 const PORT = 2620;
 const express = require('express');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 const app = express();
+const PATH_TO_OUT = 'out/pdf/'
+const PDFDocument = require('pdfkit');
 
+init()
 
 // ## Middleware Section ------------------------------
 app.use(bodyParser.json());
@@ -10,7 +17,30 @@ app.use(bodyParser.json());
 
 
 // ## Endpoints  --------------------------------------
-app.post('/convert-to-pdf', (req, res) => {
+app.all('/convert-to-pdf', (req, res) => {
+    const doc = new PDFDocument({font: 'Courier'});
+    // const doc = new PDFDocument({font: 'Consolas'});
+
+    // Pipe the PDF content to a writable stream
+    const stream = fs.createWriteStream(`${PATH_TO_OUT}\\test.pdf`);
+
+    // Pipe the PDF document to the stream
+    doc.pipe(stream);
+
+    // Add content to the PDF
+    doc.fontSize(20).text('Hello, PDF!', { align: 'center' });
+    
+    doc.fontSize(14).text('This is another line here', { align: 'right' });
+    doc.fontSize(14).text('There are Tons of things one can do with this tool', { align: 'left' });
+
+
+    // Finalize the PDF
+    doc.end();
+
+    // Notify once the PDF is generated
+    stream.on('finish', () => {
+        console.log('PDF file generated successfully.');
+    });
     res.send('Converting to PDF');
 });
 // ## Endpoints  ======================================
@@ -19,3 +49,11 @@ app.post('/convert-to-pdf', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 });
+
+
+function init() {
+    let pth = `${__dirname}\\${PATH_TO_OUT}`
+    if (fs.existsSync(pth) === false) {
+        fs.mkdirSync(pth, { recursive: true })
+    }
+}
